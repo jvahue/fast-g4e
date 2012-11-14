e75c2863e613cf406983e0eb4ff93ad11052b715
s2Norwood <s2Norwood@localhost>
2012-11-14 16:14:23 -0500

    Code Review EngineRun & Evaluator

--- G4_CP/application/EngineRun.c
+++ G4_CP/application/EngineRun.c
@@ -9,7 +9,7 @@
     Description:
 
    VERSION
-      $Revision: 35 $  $Date: 11/13/12 6:27p $
+      $Revision: 36 $  $Date: 11/14/12 12:25p $
 ******************************************************************************/
 
 /*****************************************************************************/
@@ -107,8 +107,8 @@ static void EngRunWriteRunLog   ( ER_REASON reason, ENGRUN_DATA* pErData );
  *****************************************************************************/
 ER_STATE EngRunGetState(ENGRUN_INDEX idx, UINT8* engRunFlags)
 {
-  UINT8 i;
-  UINT8 runMask  = 0x00;  // bit map of started/running EngineRuns.
+  UINT32   i;
+  UINT32   runMask;  // bit map of started/running EngineRuns.
   ER_STATE state = ER_STATE_STOPPED;
   BOOLEAN  bRunning  = FALSE;
   BOOLEAN  bStarting = FALSE;
@@ -117,20 +117,20 @@ ER_STATE EngRunGetState(ENGRUN_INDEX idx, UINT8* engRunFlags)
   // it in the provided location.
   if ( ENGRUN_ID_ANY == idx )
   {
-    runMask = 0x00;
+    runMask = 0;
 
     for (i = 0; i < MAX_ENGINES; ++i)
     {
       if (ER_STATE_RUNNING == m_engineRunData[i].erState)
       {
         // Set the bit in the flag
-        runMask |= 1 << i;
+        runMask |= 1u << i;
         bRunning = TRUE;
       }
       else if (ER_STATE_STARTING == m_engineRunData[i].erState)
       {
         // Set the bit in the flag
-        runMask |= 1 << i;
+        runMask |= 1u << i;
         bStarting = TRUE;
       }
     }
@@ -142,7 +142,7 @@ ER_STATE EngRunGetState(ENGRUN_INDEX idx, UINT8* engRunFlags)
     // If the caller has passed a field to return the list, fill it.
     if(NULL != engRunFlags)
     {
-      *engRunFlags = runMask;
+      *engRunFlags = (UINT8)(runMask & 0x000000FF);
     }
   }
   else // Get the current state of the requested engine run.
@@ -378,7 +378,7 @@ UINT16 EngRunGetBinaryHeader ( void *pDest, UINT16 nMaxByteSize )
 
    // Loop through all the triggers
    for ( engineIndex = 0;
-         ((engineIndex < MAX_ENGINES) && (nRemaining > sizeof (engineHdr[engineIndex])));
+         ((engineIndex < MAX_ENGINES) && (nRemaining > sizeof (ENGRUN_HDR)));
          engineIndex++ )
    {
       // Copy the Engine Run names
@@ -392,8 +392,8 @@ UINT16 EngRunGetBinaryHeader ( void *pDest, UINT16 nMaxByteSize )
       engineHdr[engineIndex].stopTrigID  = m_engineRunCfg[engineIndex].stopTrigID;
 
       // Increment the total number of bytes and decrement the remaining
-      nTotal     += sizeof (engineHdr[engineIndex]);
-      nRemaining -= sizeof (engineHdr[engineIndex]);
+      nTotal     += sizeof (ENGRUN_HDR);
+      nRemaining -= sizeof (ENGRUN_HDR);
    }
    // Copy the Trigger header to the buffer
    memcpy ( pBuffer, engineHdr, nTotal );
@@ -758,8 +758,6 @@ static void EngRunStartLog( const ENGRUN_CFG* pErCfg, ENGRUN_DATA* pErData )
     pSnsr = &(pErData->snsrSummary[i]);
 
     pSnsr->bValid = SensorIsValid(pSnsr->SensorIndex);
-    // todo DaveB is this really necessary ? it will be
-	  // updated anyway during EngRunUpdateLog
     if(pSnsr->bValid)
     {
       pSnsr->fMaxValue = SensorGetValue(pSnsr->SensorIndex);
@@ -1059,6 +1057,11 @@ static void EngRunUpdateStartData( const ENGRUN_CFG* pErCfg,
  *  MODIFICATIONS
  *    $History: EngineRun.c $
  * 
+ * *****************  Version 36  *****************
+ * User: Contractor V&v Date: 11/14/12   Time: 12:25p
+ * Updated in $/software/control processor/code/application
+ * Code Review corrected loss of precision warning
+ *
  * *****************  Version 35  *****************
  * User: Contractor V&v Date: 11/13/12   Time: 6:27p
  * Updated in $/software/control processor/code/application
