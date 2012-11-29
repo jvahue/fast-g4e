891bd37a1a7479b5fcad66034288765cf9c4a183
jvNorwood <jvNorwood@localhost>
2012-11-28 20:30:48 -0500

    Build for 7c1b

--- G4_CP/drivers/Version.h
+++ G4_CP/drivers/Version.h
@@ -26,7 +26,7 @@
 #define VER_FUNC    "2"       // FN - Functional Build
 #define VER_MAJOR   "0"       // MJ - Major Functional Change
 #define VER_MINOR   "0"       // MN - Minor Functional Change
-#define DEV_BUILD   " Dev 6187d7a28255"  // ER - Development Build = "" for release
+#define DEV_BUILD   " Dev de821ee67c1b"  // ER - Development Build = "" for release
 
 /******************************************************************************
                                  Package Typedefs
